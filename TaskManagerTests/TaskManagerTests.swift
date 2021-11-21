4ac5217b0de2d9cdfb06c33ae1664469ceb85bd1
Dee <writeodus@yahoo.com>
2021-11-21 09:59:48 +0000

    added test bundle, created a failing test for checking if same task can be added twice

---
+++ TaskManagerTests/TaskManagerTests.swift
@@ -0,0 +1,42 @@
+//
+//  TaskManagerTests.swift
+//  TaskManagerTests
+//
+//  Created by Dee Odus on 21/11/2021.
+//
+
+import XCTest
+@testable import TaskManager
+
+class TaskManagerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+	//failing test
+	func testSameTaskCannotSaveTwice(){
+		
+		//1. create taskManager
+		let taskManager = TaskManager.init()
+
+		//2. create a task
+		let task = Task.init(name: "Visit Friend", description: "See Ray on Monday")
+		
+		//3. add the task twice
+		_ = taskManager.add(task: task)
+		_ = taskManager.add(task: task)
+		
+		//4. get task count
+		let taskCount = taskManager.taskCount
+		
+		//5. failing test, task added twice so asserting its equal to 2
+		XCTAssertEqual(taskCount, 2)
+		
+	}
+
+}
